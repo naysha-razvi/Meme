1ab96c91a6ba078a5c79de106a044ac45b2269ec
naysha-razvi <naysharazvi@gmail.com>
2026-09-23 15:26:58 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -23,7 +23,7 @@ except Exception:
 
 client = Groq(api_key=GROQ_API_KEY)
 
-MODEL_NAME = "llama-3.1-8b-instant"
+MODEL_NAME = "openai/gpt-oss-20b"
 
 # ==================================================
 # SYSTEM PROMPT
